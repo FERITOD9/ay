1a937872bc2d1aaba655c1b788daca358efadf70
FERITOD9 <92444394+FERITOD9@users.noreply.github.com>
2021-10-24 17:16:13 +0700

    Update help.js

--- message/help.js
+++ message/help.js
@@ -227,7 +227,7 @@ LIST HARGA SEWA BOTZ
 ▢ PERBULAN 15K
 ▢ PERMANEN 40K
 MINAT HIBUNGI OWNER
-wa.me/6282287486762
+wa.me/6283129083971
 
 YG MAU TOP UP GAME MARI JOIN
 https://chat.whatsapp.com/KUb8JJwppqL3zLS7p9xXx7`
@@ -438,9 +438,9 @@ exports.allMenu = (prefix, cmhit, runtime, pushname, isOwner, isPremium, gcount,
 ❏ LIMIT : *${isPremium ? 'Unlimited' : `${gcount}`}*
 
 𝗜𝗡𝗙𝗢 𝗕𝗢𝗧
-❏ NAMA : *DHA BOTZ*
-❏ API : @6282288265627
-❏ OWNER : *YUDHA PERDANA*
+❏ NAMA : *FERI BOTZ*
+❏ API : @6283129083971
+❏ OWNER : *FERI STORE*
 ❏ API : *@6282W87486762*
 ❏ TOTAL HIT : *${cmhit.length}x HIT*
 ❏ AKTIF : *${runtime(process.uptime())}*
